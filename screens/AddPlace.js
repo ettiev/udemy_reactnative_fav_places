302a24c382505ac5e8a45cd6f40ad7baab86addb
ettiev <126219891+ettiev@users.noreply.github.com>
2024-09-19 16:04:04 +0200

    Added Screens and Components
    - Added AddPlace screen
    - Added ApplPlaces screen
    - Added Map screen
    - Added PlacesDetails screen
    - Added Colors constant (provided by instructor)
    - Added IconButton
    - Added PlaceForm
    - Added PlaceItem
    - Added PlaceList

---
+++ screens/AddPlace.js
@@ -0,0 +1,13 @@
+// npm install @react-navigation/native
+// npx expo install react-native-screens react-native-safe-area-context
+// npm install @react-navigation/native-stack
+
+import PlaceForm from "../components/Places/PlaceForm";
+
+function AddPlace() {
+    return (
+        <PlaceForm />    
+    )
+}
+
+export default AddPlace;
